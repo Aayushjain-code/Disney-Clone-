724548b95cdb4eadc64178e1c164234f7f44df14
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-10 17:10:50 +0530

    Finished Login Functionality & Redirect User

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
-import { configureStore, getDefaulMiddleware } from '@reduxjs/toolkit'
-
-
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import userReducer from "../features/user/userSlice";
+// import movieReducer from "../features/movie/movieSlice";
 
 export default configureStore({
 	reducer: {
 		user: userReducer,
-		movie: movieReducer,
+		// movie: movieReducer,
 	},
 	middleware: getDefaultMiddleware({
 		serializableCheck: false,
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,97 @@
-import styled from 'styled-components';
+import { useEffect } from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { auth, provider } from "../firebase";
+import {
+   selectUserName,
+   selectUserPhoto,
+   setUserLoginDetails,
+   setSignOutState,
+} from "../features/user/userSlice";
+
 const Header = (props) => {
+   const dispatch = useDispatch();
+   const history = useHistory();
+   const userName = useSelector(selectUserName);
+   const userPhoto = useSelector(selectUserPhoto);
+
+   useEffect(() => {
+      auth.onAuthStateChanged(async (user) => {
+         if (user) {
+            setUser(user);
+            history.push("/home");
+         }
+      });
+   }, [userName]);
 
    const handleAuth = () => {
-      auth.signInWithPopup(provider).then((result) => {
-         console.log(result);
-      }).catch((error) => {
-         alert(error.message);
-      });
+      auth.signInWithPopup(provider)
+         .then((result) => {
+            setUser(result.user);
+         }).catch((error) => {
+            alert(error.message);
+         });
    }
+
+
+   const setUser = (user) => {
+      dispatch(
+         setUserLoginDetails({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+         })
+      );
+   };
+
    return (
       <Nav>
          <Logo>
-            <img src="./images/logo.svg" alt="Disney-Clone" />
+            <img src="/images/logo.svg" alt="Disney+" />
          </Logo>
-         <NavMenu>
-            <a href="/home">
-               <img src="/images/home-icon.svg" alt="HOME" />
-               <span>HOME</span>
-            </a>
-            <a>
-               <img src="/images/search-icon.svg" alt="SEARCH" />
-               <span>SEARCH</span>
-            </a>
-            <a>
-               <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-               <span>WATCHLIST</span>
-            </a>
-            <a>
-               <img src="/images/original-icon.svg" alt="ORIGINALS" />
-               <span>ORIGINALS</span>
-            </a>
-            <a>
-               <img src="/images/movie-icon.svg" alt="MOVIES" />
-               <span>MOVIES</span>
-            </a>
-            <a>
-               <img src="/images/series-icon.svg" alt="SERIES" />
-               <span>SERIES</span>
-            </a>
-         </NavMenu>
-         <Login onClick={handleAuth}>Login</Login>
+
+         {!userName ? (
+            <Login onClick={handleAuth}>Login</Login>) : (
+            <>
+               <NavMenu>
+                  <a href="/home">
+                     <img src="/images/home-icon.svg" alt="HOME" />
+                     <span>HOME</span>
+                  </a>
+                  <a>
+                     <img src="/images/search-icon.svg" alt="SEARCH" />
+                     <span>SEARCH</span>
+                  </a>
+                  <a>
+                     <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                     <span>WATCHLIST</span>
+                  </a>
+                  <a>
+                     <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                     <span>ORIGINALS</span>
+                  </a>
+                  <a>
+                     <img src="/images/movie-icon.svg" alt="MOVIES" />
+                     <span>MOVIES</span>
+                  </a>
+                  <a>
+                     <img src="/images/series-icon.svg" alt="SERIES" />
+                     <span>SERIES</span>
+                  </a>
+               </NavMenu>
+               <SignOut>
+                  <UserImg src={userPhoto} alt={userName} />
+                  <DropDown>
+                     <span onClick={handleAuth}>Sign out</span>
+                  </DropDown>
+               </SignOut>
+            </>
+         )}
       </Nav>
-   )
-}
+   );
+};
+
 
 const Nav = styled.nav`
   position: fixed;
@@ -132,7 +180,6 @@ const NavMenu = styled.div`
   } */
 `;
 
-
 const Login = styled.a`
   background-color: rgba(0, 0, 0, 0.6);
   padding: 8px 16px;
@@ -148,8 +195,44 @@ const Login = styled.a`
   }
 `;
 
+const UserImg = styled.img`
+  height: 100%;
+`;
 
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
 
-
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
 
 export default Header;
